111d4524e5f0c8ca9ef0d06a058f9a97bf6f7070
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-03-04 18:00:28 +0100

    Add example main: dragon

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,16 @@
 #include <SFML/Graphics.hpp>
 #include "grammar/Grammar.h"
+#include "engine/Engine.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    Engine eng(1920, 1080);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-     /* Grammar test
-      *
     Grammar g("FX", 30);
     g.add_rule('Y', "-FX-Y");
     g.add_rule('X', "X+YF+");
-    std::cout << g.generate(3) << std::endl;
-      */
+
+    eng.render(eng.draw(g.generate(10), 90, 10));
 
     return 0;
 }
