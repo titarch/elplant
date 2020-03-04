17b3a26003ac43b86cbdeb06e63265603d10d429
Matthew Rousseau <matthew.rousseau@epita.fr>
2020-03-04 18:46:57 +0100

    Get rule with weighted uniform distribution

--- src/grammar/Grammar.cpp
+++ src/grammar/Grammar.cpp
@@ -4,9 +4,36 @@
 
 #include "Grammar.h"
 
-void Grammar::add_rule(char lvalue, const String &rvalue) {
-    rules_.insert({lvalue, rvalue});
+void Rule::add_rule(const String &rvalue, unsigned weight) {
+    rvalues_.push_back(rvalue);
+    weights_.push_back(weight);
 
+    update_summed_weights();
+}
+
+String Rule::get_rule() const {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> uni(0, weight_sum_ - 1);
+    unsigned rnd = uni(gen);
+    for(unsigned i = 0; i < weights_.size(); i++) {
+        if(rnd < weights_[i])
+            return rvalues_[i];
+        rnd -= weights_[i];
+    }
+    return "";
+}
+
+void Rule::update_summed_weights() {
+    weight_sum_ = std::accumulate(weights_.begin(), weights_.end(), 0u);
+}
+
+void Grammar::add_rule(char lvalue, const String &rvalue, unsigned weight) {
+    auto found_rule = rules_.find(lvalue);
+    if (found_rule == rules_.end())
+        rules_.insert({lvalue, Rule(lvalue, rvalue)});
+    else
+        found_rule->second.add_rule(rvalue, weight);
 }
 
 String Grammar::generate(int n) const {
@@ -32,6 +59,6 @@ void Grammar::generate_rec(String &buffer, const String &cur_rule, int max_rec,
         if (found_rule == rules_.end())
             buffer.push_back(cur_rule[i]);
         else
-            generate_rec(buffer, found_rule->second, max_rec, cur_rec + 1);
+            generate_rec(buffer, found_rule->second.get_rule(), max_rec, cur_rec + 1);
     }
 }
--- src/grammar/Grammar.h
+++ src/grammar/Grammar.h
@@ -7,22 +7,51 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 #include <map>
+#include <random>
 
 using String = std::string;
+
+class Rule {
+public:
+    Rule(char lvalue, const String &rvalue, unsigned weight = 1): lvalue_(lvalue)
+    {
+        rvalues_ = std::vector<String>();
+        rvalues_.push_back(rvalue);
+
+        weights_ = std::vector<unsigned>();
+        weights_.push_back(weight);
+
+        update_summed_weights();
+    }
+
+    void add_rule(const String &rvalue, unsigned weight = 1);
+    String get_rule() const;
+
+private:
+    char lvalue_;
+    std::vector<String> rvalues_;
+    std::vector<unsigned> weights_;
+
+    unsigned weight_sum_ = 0;
+
+    void update_summed_weights();
+};
+
 
 class Grammar {
 public:
     Grammar(const String axiom, float angle): axiom_(axiom), angle_(angle) {}
 
-    void add_rule(char lvalue, const String &rvalue);
+    void add_rule(char lvalue, const String &rvalue, unsigned weight = 1);
     String generate(int n) const;
 
 private:
     void generate_rec(String &buffer, const String &cur_rule, int max_rec, int cur_rec) const;
 
     const String axiom_;
-    std::map<char, String> rules_;
+    std::map<char, Rule> rules_;
     float angle_;
 };
 
