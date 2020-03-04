3478c06a565dcfb2a98abfdd466cf77b2815f0e9
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-03-04 17:55:45 +0100

    Implement first engine

--- src/engine/Engine.h
+++ src/engine/Engine.h
@@ -5,9 +5,31 @@
 #ifndef ELPLANT_ENGINE_H
 #define ELPLANT_ENGINE_H
 
+#include <string>
+#include <SFML/Graphics.hpp>
+#include "../utils/Vector.h"
+
+using line = std::pair<sf::Vertex, sf::Vertex>;
+using lines = std::vector<line>;
+
+struct Turtle {
+    Vec2f o;
+    double d;
+
+    Turtle(const Vec2f& o, double d) : o(o), d(d) {}
+    friend std::ostream& operator<<(std::ostream& os, Turtle const& t) {
+        return os << t.o << ' ' << t.d << "°";
+    }
+};
 
 class Engine {
+public:
+    Engine(unsigned width, unsigned height) : width_(width), height_(height) {}
 
+    [[nodiscard]] lines draw(std::string const& s, double angle, double length) const;
+    void render(lines const& lines) const;
+protected:
+    unsigned width_, height_;
 };
 
 
