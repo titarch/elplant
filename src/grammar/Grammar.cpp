9f2a0981ddb589de61a82490e7b78ee2054e3cd7
Matthew Rousseau <matthew.rousseau@epita.fr>
2020-03-04 16:21:38 +0100

    Add Grammar class and generation of strings

--- src/grammar/Grammar.cpp
+++ src/grammar/Grammar.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "Grammar.h"
+
+void Grammar::add_rule(char lvalue, const String &rvalue) {
+    rules_.insert({lvalue, rvalue});
+
+}
+
+String Grammar::generate(int n) const {
+    String buffer;
+    generate_rec(buffer, axiom_, n, 0);
+
+    return buffer;
+}
+
+void Grammar::generate_rec(String &buffer, const String &cur_rule, int max_rec,
+                             int cur_rec) const {
+    if (cur_rec >= max_rec)
+    {
+        buffer.append(cur_rule);
+        return;
+    }
+    for (unsigned i = 0; i < cur_rule.size(); i++)
+    {
+        auto found_rule = rules_.find(cur_rule[i]);
+        if (found_rule == rules_.end())
+            buffer.push_back(cur_rule[i]);
+        else
+            generate_rec(buffer, found_rule->second, max_rec, cur_rec + 1);
+    }
+}
--- src/grammar/Grammar.h
+++ src/grammar/Grammar.h
@@ -5,9 +5,25 @@
 #ifndef ELPLANT_GRAMMAR_H
 #define ELPLANT_GRAMMAR_H
 
+#include <iostream>
+#include <string>
+#include <map>
+
+using String = std::string;
 
 class Grammar {
+public:
+    Grammar(const String axiom, float angle): axiom_(axiom), angle_(angle) {}
 
+    void add_rule(char lvalue, const String &rvalue);
+    String generate(int n) const;
+
+private:
+    void generate_rec(String &buffer, const String &cur_rule, int max_rec, int cur_rec) const;
+
+    const String axiom_;
+    std::map<char, String> rules_;
+    float angle_;
 };
 
 
