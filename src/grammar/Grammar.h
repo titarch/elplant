21b6e7efb4308c335fef999ee5c30ce027356cc4
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-03-05 13:38:32 +0100

    Refactor Grammar header

--- src/grammar/Grammar.h
+++ src/grammar/Grammar.h
@@ -8,18 +8,19 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <utility>
 
 using String = std::string;
 
 class Grammar {
 public:
-    Grammar(const String axiom, float angle): axiom_(axiom), angle_(angle) {}
+    Grammar(String&& axiom, float angle) : axiom_(axiom), angle_(angle) {}
 
-    void add_rule(char lvalue, const String &rvalue);
-    String generate(int n) const;
+    void add_rule(char lvalue, const String& rvalue);
+    [[nodiscard]] String generate(int n) const;
 
 private:
-    void generate_rec(String &buffer, const String &cur_rule, int max_rec, int cur_rec) const;
+    void generate_rec(String& buffer, const String& cur_rule, int max_rec, int cur_rec) const;
 
     const String axiom_;
     std::map<char, String> rules_;
