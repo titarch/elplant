dfbc18e4cd85ee46d30718f434f72ceaf529e7d7
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-05-01 17:09:47 +0200

    Add first implem of parametric grammars

--- src/grammar/Grammar.cpp
+++ src/grammar/Grammar.cpp
@@ -4,7 +4,7 @@
 
 #include "Grammar.h"
 
-void Rule::add_rule(const String &rvalue, unsigned weight) {
+void Rule::add_rule(const String& rvalue, unsigned weight) {
     rvalues_.push_back(rvalue);
     weights_.push_back(weight);
 
@@ -14,8 +14,8 @@ void Rule::add_rule(const String &rvalue, unsigned weight) {
 String Rule::get_rule() const {
     // FIXME: Use better random function
     unsigned rnd = (rand() % (weight_sum_));
-    for(unsigned i = 0; i < weights_.size(); i++) {
-        if(rnd < weights_[i])
+    for (unsigned i = 0; i < weights_.size(); i++) {
+        if (rnd < weights_[i])
             return rvalues_[i];
         rnd -= weights_[i];
     }
@@ -26,7 +26,7 @@ void Rule::update_summed_weights() {
     weight_sum_ = std::accumulate(weights_.begin(), weights_.end(), 0u);
 }
 
-void Grammar::add_rule(char lvalue, const String &rvalue, unsigned weight) {
+void Grammar::add_rule(char lvalue, const String& rvalue, unsigned weight) {
     auto found_rule = rules_.find(lvalue);
     if (found_rule == rules_.end())
         rules_.insert({lvalue, Rule(lvalue, rvalue)});
@@ -41,22 +41,86 @@ String Grammar::generate(int n) const {
     return buffer;
 }
 
-void Grammar::generate_rec(String &buffer, const String &cur_rule, int max_rec,
-                             int cur_rec) const {
-    if (cur_rec >= max_rec)
-    {
+void Grammar::generate_rec(String& buffer, const String& cur_rule, int max_rec,
+                           int cur_rec) const {
+    if (cur_rec >= max_rec) {
         if (buffer.size() + cur_rule.size() > buffer.capacity())
             buffer.reserve(buffer.size() * 2);
         buffer.append(cur_rule);
 
         return;
     }
-    for (unsigned i = 0; i < cur_rule.size(); i++)
-    {
+    for (unsigned i = 0; i < cur_rule.size(); i++) {
         auto found_rule = rules_.find(cur_rule[i]);
         if (found_rule == rules_.end())
             buffer.push_back(cur_rule[i]);
         else
             generate_rec(buffer, found_rule->second.get_rule(), max_rec, cur_rec + 1);
     }
+}
+
+String ParamRule::substitute(String const& s, std::vector<double> const& values) {
+    String os;
+    for (auto const& c : s) {
+        if (auto it = std::find(params_.cbegin(), params_.cend(), c); it != params_.cend()) {
+            os += std::to_string(values[it - params_.cbegin()]);
+        } else
+            os.push_back(c);
+    }
+    return os;
+}
+
+String ParamRule::evaluate(std::vector<double> const& values_) {
+    for (auto const& c : crs_)
+        if (c.evaluate(values_[c.lpos]))
+            return substitute(c.rvalue, values_);
+    throw std::runtime_error("No condition matched...");
+}
+
+String ParamGrammar::generate_rec(String const& in, int cur_rec, int max_rec) {
+    String out{};
+    for (auto c = in.cbegin(); c != in.cend();) {
+        if (rules_.contains(*c)) {
+            auto& rule = rules_.at(*c);
+            ++c;
+            Strings exps;
+            if (c != in.cend() && *c == '(') {
+                String exp{};
+                ++c;
+                for (; *c != ')'; ++c) {
+                    if (*c == ',') {
+                        exps.push_back(exp);
+                        exp.clear();
+                    } else {
+                        exp.push_back(*c);
+                    }
+                }
+                exps.push_back(exp);
+                ++c;
+            }
+            std::vector<double> value = evaluate(exps);
+            out += rule.evaluate(value);
+        } else {
+            out.push_back(*c);
+            ++c;
+        }
+    }
+    if (cur_rec == max_rec) return out;
+    return generate_rec(out, cur_rec + 1, max_rec);
+}
+
+double evaluate(String const& s) {
+    static exprtk::expression<double> expr;
+    static exprtk::parser<double> parser;
+    if (!parser.compile(s, expr))
+        throw std::runtime_error(s + ": parsing error");
+    return expr.value();
+}
+
+std::vector<double> evaluate(Strings const& exps) {
+    std::vector<double> v{};
+    v.reserve(exps.size());
+    std::transform(exps.cbegin(), exps.cend(), std::back_inserter(v),
+                   [](String const& s) -> double { return evaluate(s); });
+    return v;
 }
--- src/grammar/Grammar.h
+++ src/grammar/Grammar.h
@@ -12,8 +12,10 @@
 #include <map>
 #include <utility>
 #include <random>
+#include "exprtk.hpp"
 
 using String = std::string;
+using Strings = std::vector<String>;
 
 class Rule {
 public:
@@ -40,12 +42,10 @@ private:
     void update_summed_weights();
 };
 
-
 class Grammar {
 public:
-    explicit Grammar(String axiom) : axiom_(std::move(axiom))
-    {
-        srand( (unsigned)time(0) );
+    explicit Grammar(String axiom) : axiom_(std::move(axiom)) {
+        srand((unsigned) time(0));
     }
 
     void add_rule(char lvalue, const String& rvalue, unsigned weight = 1);
@@ -57,5 +57,99 @@ private:
     std::map<char, Rule> rules_;
 };
 
+
+enum class Op {
+    LT,
+    LE,
+    GT,
+    GE,
+    EQ,
+    NE,
+    TRUE,
+    FALSE
+};
+
+struct Condition {
+    Op op;
+    double rval;
+
+    Condition(Op op, double rval) : op(op), rval(rval) {}
+
+    [[nodiscard]] bool evaluate(double var) const {
+        switch (op) {
+            case Op::LT:
+                return var < rval;
+            case Op::LE:
+                return var <= rval;
+            case Op::GT:
+                return var > rval;
+            case Op::GE:
+                return var >= rval;
+            case Op::EQ:
+                return var == rval;
+            case Op::NE:
+                return var != rval;
+            case Op::TRUE:
+                return true;
+            case Op::FALSE:
+                return false;
+        }
+        return false;
+    }
+};
+
+struct ConditionRule {
+    Condition c;
+    size_t lpos;
+    String rvalue;
+
+    ConditionRule(const Condition& c, size_t lpos, String rvalue) : c(c), lpos(lpos), rvalue(std::move(rvalue)) {}
+
+    [[nodiscard]] bool evaluate(double var) const {
+        return c.evaluate(var);
+    }
+};
+
+class ParamRule {
+public:
+    explicit ParamRule(std::vector<char> params) : params_(std::move(params)), crs_() {}
+
+    ParamRule& add_conditional_rule(char param, Op op, double value, String const& rvalue) {
+        auto it = std::find(params_.cbegin(), params_.cend(), param);
+        if (it == params_.cend()) throw std::invalid_argument(String("Parameter ") + param + " was not found");
+        crs_.emplace_back(Condition{op, value}, it - params_.cbegin(), rvalue);
+        return *this;
+    }
+
+    String evaluate(std::vector<double> const& values_);
+
+private:
+    String substitute(const String& s, const std::vector<double>& values);
+    std::vector<char> params_;
+    std::vector<ConditionRule> crs_;
+};
+
+class ParamGrammar {
+public:
+    explicit ParamGrammar(String axiom) : axiom_(std::move(axiom)), rules_() {}
+
+    ParamGrammar& add_rule(char c, ParamRule const& pr) {
+        rules_.insert({c, pr});
+        return *this;
+    }
+
+    String generate(int n) {
+        return generate_rec(axiom_, 0, n);
+    }
+
+private:
+    String generate_rec(String const& in, int cur_rec, int max_rec);
+
+    String axiom_;
+    std::map<char, ParamRule> rules_;
+};
+
+double evaluate(String const& s);
+std::vector<double> evaluate(Strings const& exps);
 
 #endif //ELPLANT_GRAMMAR_H
