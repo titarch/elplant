35fc3bbfa0cde671e6ba275c374e0fe94e2edb97
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-05-03 10:31:45 +0200

    Fix leaf generation in raytracer yaml

--- src/engine/Shapes.cpp
+++ src/engine/Shapes.cpp
@@ -122,7 +122,8 @@ Mesh Leaf::to_mesh() const {
 
 TriangleMesh Leaf::to_triangles() const {
     TriangleMesh tm{};
-    for (auto i = 1u; i < !vertices.empty() && vertices.size() - 1; ++i)
+    if (vertices.empty()) return tm;
+    for (auto i = 1u; i < vertices.size() - 1; ++i)
         tm.emplace_back(vertices[0], vertices[i], vertices[i + 1], color_index);
     return tm;
 }
